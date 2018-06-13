7e0b49e8a68079232490fd5a753c1d90ee7cb7e4
Blair MacNeil <macneib@gmail.com>
2018-06-13 07:31:50 -0400

    fixed broken test

--- src/api/transaction/transaction.integration.js
+++ src/api/transaction/transaction.integration.js
@@ -26,20 +26,22 @@ import mongoose from 'mongoose';
 import ethereumAddress from 'ethereum-address';
 
 async function genWallet() {
-  return await Wallet.create({
-    _id: '5b17f7a059ca190014773f8c',
-    userId: '1',
-    address: '0xcad96402cB0612C173e850DFfe60c0a606c9fEB7',
-    privateKey:
-      '3e90e479c03d2948f2609d2657a3ce4d37838ea742ddbcdf817c04e2604d6691c741f8f995cc58694db60179f9e4eeea702e39519289aa36b626071cb2460c86ce4fb8a0ef1ee446f14d9a98cf06118e'
-  },
+  return await Wallet.create(
+    {
+      _id: '5b17f7a059ca190014773f8c',
+      userId: '1',
+      address: '0xcad96402cB0612C173e850DFfe60c0a606c9fEB7',
+      privateKey:
+        '3e90e479c03d2948f2609d2657a3ce4d37838ea742ddbcdf817c04e2604d6691c741f8f995cc58694db60179f9e4eeea702e39519289aa36b626071cb2460c86ce4fb8a0ef1ee446f14d9a98cf06118e'
+    },
     {
       _id: '5b17f7b559ca19001477408e',
       userId: '2',
       address: '0x69833CF0D1324aA939EaF0AD2424ECcCc3d03D73',
       privateKey:
         'f8d756728853484c828e03901044aa55ced992f21edc3841dac30e265a39af09f609327a8e00ad0a7d188b84ce40246f25a9fe99fbf5f7da3fa08a05f378349a31896c775b3ab39142693a668f9369e2'
-    });
+    }
+  );
 }
 
 describe('Transaction API:', () => {
@@ -66,8 +68,10 @@ describe('Transaction API:', () => {
         .post('/api/v1/transactions')
         .set('x-api-key', `${config.key}`)
         .send({
-          fromWalletId: '5b17f7a059ca190014773f8c',
-          toWalletId: '5b17f7b559ca19001477408e',
+          account: 1,
+          txId: '5b17f7a059ca190014773f8d',
+          from: '5b17f7a059ca190014773f8c',
+          to: '5b17f7b559ca19001477408e',
           amount: '1000'
         })
         .expect(201)
